dc0d26c4d0c6c7086ab85f9f1de345808960b393
yuanxy92 <yuanxy92@gmail.com>
2018-12-07 14:46:30 +0000

    modify cpp

--- getFrame.cpp
+++ getFrame.cpp
@@ -39,11 +39,12 @@ inline int getOutFileID(int camId){
 	char temp[10];
 	sprintf(temp, "%d", camId);
 	int l = strlen(temp);
-        if(l < 3) return camId;
-        int i = 1;
+    if(l < 3) 
+        return camId;
+    int i = 1;
 	for(; i < l; i++){
 		if(temp[i] != 0) break;
-        }
+    }
 	return atoi(temp+i);
 }
 
@@ -54,19 +55,17 @@ void mcamFrameCallback(FRAME frame, void* data)
 {
 	if(frame.m_metadata.m_tile == 0){ //when acosd starts with "-s 2", select differenct scales, 0: 3864x2174; 1:1920x1080, otherwise, only 0 is available
 		FILE** fileH = static_cast<FILE**>(data);
-		//if(frame.m_metadata.m_camId == 1){ //select camera
-		//}
 		fwrite(frame.m_image, 1, frame.m_metadata.m_size, fileH[getOutFileID(frame.m_metadata.m_camId)]);
-
+		fwrite(&frame.m_metadata, 1, sizeof(frame.m_metadata), fileH[getOutFileID(frame.m_metadata.m_camId) + 19]);
 	}
 }
 
 void printHelp()
 {
-   printf("Get frame stream:\n");
-   printf("Usage:\n");
-   printf("\t<Client Port> port connect from(default 13000)\n\n");
-   printf("\t<Server Port> port connect to (default 9998)\n\n");
+    printf("Get frame stream:\n");
+    printf("Usage:\n");
+    printf("\t<Client Port> port connect from(default 13000)\n\n");
+    printf("\t<Server Port> port connect to (default 9998)\n\n");
 }
 
 int connectToIpsFromSyncFile(char fileName[], int sPort)
@@ -97,6 +96,8 @@ int connectToIpsFromSyncFile(char fileName[], int sPort)
 }
 
 int main(int argc, char* argv[]){
+    /****************   read configuration  *****************/ 
+    /********************************************************/
     //Parse arguments
     if(argc >1 && strcasecmp(argv[1],"-h") == 0) {
 	printHelp();
@@ -105,7 +106,7 @@ int main(int argc, char* argv[]){
     char* hostfile = "sync.cfg";
     int cPort = 13000;
     if(argc > 1) cPort = atoi(argv[1]);
-    int sPort = 9999;
+    int sPort = 9998;
     if(argc > 2) sPort = atoi(argv[2]);
     printf("Server Port: %d\nClient Port: %d\n", sPort, cPort);
 
@@ -128,7 +129,6 @@ int main(int argc, char* argv[]){
      * has already been discovered at the time of setting the callback */
     setNewMCamCallback(mcamCB);
 
-
     /* Next we set a callback function to receive the stream of frames
      * from the desired microcamera */
     MICRO_CAMERA_FRAME_CALLBACK frameCB;
@@ -136,14 +136,24 @@ int main(int argc, char* argv[]){
 
     //Set output file handles
     char fileName[20] = {0};
-    vOutFiles.resize(numMCams); 
+    char fileNameConfig[20] = {0};
+    vOutFiles.resize(numMCams * 2); 
+
     for (int i = 0; i < numMCams; i++){
-	printf("CameraId: %d\n", mcamList[i].mcamID);
-	sprintf(fileName,"frameData_%d", mcamList[i].mcamID);
-	int fileId = getOutFileID(mcamList[i].mcamID);
-        if(fileId >= vOutFiles.size()) vOutFiles.resize(fileId + 1);
-	vOutFiles[fileId] = fopen(fileName, "wb+");
-	printf("Camera %d saved to %s fileID:%d\n", mcamList[i].mcamID, fileName, fileId);
+
+	    printf("CameraId: %d\n", mcamList[i].mcamID);
+	    sprintf(fileName,"mcam_%d", mcamList[i].mcamID);
+	    sprintf(fileName,"mcam_config_%d", mcamList[i].mcamID);
+	    int fileId = getOutFileID(mcamList[i].mcamID);
+
+        // if(fileId >= vOutFiles.size()) {
+        //     vOutFiles.resize((fileId + 1) * 2);
+        // }
+	    
+        vOutFiles[fileId] = fopen(fileName, "wb");
+        vOutFiles[fileId + numMCams] = fopen(fileNameConfig, "wb");
+	    printf("Camera %d saved to %s fileID:%d\n", mcamList[i].mcamID, fileName, fileId);
+	    printf("Camera config file %d saved to %s fileID:%d\n", mcamList[i].mcamID, fileNameConfig, fileId);
     }
 
     frameCB.data = (void*)&vOutFiles[0];
@@ -156,7 +166,7 @@ int main(int argc, char* argv[]){
     /*************************************************************/
     /*************************************************************/
     /* For each camera in MCamList start the stream for 10 seconds, then stop it,
-       which will allow the frame callback to recieve frames and save the timestamp
+        which will allow the frame callback to recieve frames and save the timestamp
        to a file */ 
     for (int i = 0; i < numMCams; i++){
 	    //Start the stream
@@ -164,40 +174,41 @@ int main(int argc, char* argv[]){
 		    printf("Failed to start streaming mcam %u\n", mcamList[i].mcamID);
 		    exit(0);
 	    }
-	//if(setMCamWhiteBalance( mcamList[i], 8)){
-	AtlWhiteBalance wb = getMCamWhiteBalance(mcamList[i]);
-	printf("CAM:%d before-- red: %f green: %f blue: %f\n ",mcamList[i].mcamID, wb.red, wb.green, wb.blue);
-	//if(setMCamWhiteBalanceMode( mcamList[i], 3)){
-	//if(setMCamWhiteBalance( mcamList[i], wb)){
-	//	printf("WhiteBalance successfully Set mode to: %d\n", 3);
-	//}
+	    //if(setMCamWhiteBalance( mcamList[i], 8)){
+	    AtlWhiteBalance wb = getMCamWhiteBalance(mcamList[i]);
+	    printf("CAM:%d before-- red: %f green: %f blue: %f\n ",mcamList[i].mcamID, wb.red, wb.green, wb.blue);
+	    //if(setMCamWhiteBalanceMode( mcamList[i], 3)){
+	    //if(setMCamWhiteBalance( mcamList[i], wb)){
+	    //	printf("WhiteBalance successfully Set mode to: %d\n", 3);
+	    //}
 	    sleep(0.2);
-
     }
 
     char a;
     scanf("%c", &a);
 
-printf("start to stop streaming!\n");
+    printf("start to stop streaming!\n");
 
     for (int i = 0; i < numMCams; i++){
         //Stop the stream
         if( !stopMCamStream(mcamList[i], cPort+i) ){
             printf("Failed to stop streaming mcam %u\n", mcamList[i].mcamID);
         }
-        
     }
 
 
-  for (int i = 0; i < numMCams; i++){
-    	closeMCamFrameReceiver( cPort+i );
-  }
-
- //close output file handles
     for (int i = 0; i < numMCams; i++){
-	fclose(vOutFiles[getOutFileID(mcamList[i].mcamID)]);
-		AtlWhiteBalance wb = getMCamWhiteBalance(mcamList[i]);
-		printf("CAM: %d after-- red: %f green: %f blue: %f\n",mcamList[i].mcamID, wb.red, wb.green, wb.blue);
+    	closeMCamFrameReceiver( cPort+i );
     }
-   printf("%d frames received\n", count);
+
+    //close output file handles
+    for (int i = 0; i < numMCams; i++){
+	    fclose(vOutFiles[getOutFileID(mcamList[i].mcamID)]);
+	    fclose(vOutFiles[getOutFileID(mcamList[i].mcamID) + 19]);
+    
+        AtlWhiteBalance wb = getMCamWhiteBalance(mcamList[i]);
+	    printf("CAM: %d after-- red: %f green: %f blue: %f\n",mcamList[i].mcamID, wb.red, wb.green, wb.blue);
+    }
+    
+    printf("%d frames received\n", count);
 }
