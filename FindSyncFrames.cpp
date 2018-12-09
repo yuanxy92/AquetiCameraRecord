e881a5d261c1d14a142e3d5882eaafa7179088c1
yuanxy92 <yuanxy92@gmail.com>
2018-12-09 16:55:25 +0800

    finish frame selection

--- FindSyncFrames.cpp
+++ FindSyncFrames.cpp
@@ -42,37 +42,121 @@ int extractSyncFrames(std::vector<std::vector<uint64_t>> timeStamps,
     std::vector<std::vector<int>>& selectedFrameInds) {
     // go through all the first frame to find the one having largest time stamp
     uint64_t largestTimeStamp = 0;
+    uint64_t smallestTimeStamp = timeStamps[0][0];
     for (int i = 0; i < 19; i ++) {
         if (timeStamps[i][0] > largestTimeStamp) {
             largestTimeStamp = timeStamps[i][0];
+        }
+        if (timeStamps[i][0] < smallestTimeStamp) {
+            smallestTimeStamp = timeStamps[i][0];
         }
     }
     printf("Timestamp of the first frame is %llu !\n", largestTimeStamp);
     // find inds for each h264 stream
     selectedFrameInds.resize(19);
-    uint64_t eps = 33000 / 2; 
+    uint64_t eps = 100000 / 6; 
     for (int i = 0; i < 19; i ++) {
         for (int ind = 0; ind < timeStamps[i].size(); ind ++) {
             uint64_t dist = timeStampDist(largestTimeStamp, timeStamps[i][ind]);
             if (dist < eps) {
                 if (ind < timeStamps[i].size() - 1) {
                     uint64_t dist2 = timeStampDist(largestTimeStamp, timeStamps[i][ind + 1]);
-                    if (dist < dist2)
+                    if (dist < dist2) {
                         selectedFrameInds[i].push_back(ind);
-                    else selectedFrameInds[i].push_back(ind + 1);
+                        printf("Stream %d select %dth frame\n", i, ind);
+                    }
+                    else {
+                        selectedFrameInds[i].push_back(ind + 1);
+                        printf("Stream %d select %dth frame\n", i, ind + 1);
+                    }
                 }
                 else selectedFrameInds[i].push_back(ind);
                 break;
             }
         }
     }
+
+    // init first frame
+    std::vector<int> curInds(19);
+    uint64_t startTime = smallestTimeStamp;
+    int curTime = 0;
+    int meanTime = 0;
+    for (int i = 0; i < 19; i ++) {
+        meanTime += (int)(timeStamps[i][selectedFrameInds[i][0]] - startTime);
+        curInds[i] = selectedFrameInds[i][0] + 1;
+    }
+    meanTime /= 19;
+    curTime = meanTime;
+
+    printf("Current time is %d\n", curTime);
+    
+    bool lastframe = false;
+
     // output information
     for (int i = 0; i < 19; i ++) {   
-        printf("Stream %d has %lu frames, starts from index %d, has valid frames %d\n", i, timeStamps[i].size(),
-            selectedFrameInds[i][0], timeStamps[i].size() - selectedFrameInds[i][0]);
+        printf("Stream %d has %lu frames, starts from index %d, has valid frames %d, current shift %d\n",
+            i, timeStamps[i].size(), selectedFrameInds[i][0], (int)timeStamps[i].size() - selectedFrameInds[i][0],
+                (int)(timeStamps[i][selectedFrameInds[i][0]] - startTime) - curTime);
     }
 
-    // start to find 
+    curTime += 100000 / 3;
+
+    int frameInd = 1; 
+    for (;;) {
+        // find index with the best time stamp
+        bool passsync = true;
+        for (int i = 0; i < 19; i ++) {
+            int dist;
+            uint64_t ts1 = timeStamps[i][curInds[i]] - startTime;
+            int dist1 = timeStampDist(ts1, curTime);
+            if (curInds[i] < timeStamps[i].size()) {
+                uint64_t ts2 = timeStamps[i][curInds[i] + 1] - startTime;
+                int dist2 = timeStampDist(ts2, curTime);
+                if (dist2 < dist1) {
+                    curInds[i]++;
+                    dist = dist2;
+                }
+                else dist = dist1;
+            }
+            if (dist > eps) {
+                passsync = false;
+                break;
+            }
+        }
+        // add new frame
+        if (passsync == true) {
+            printf("\nSelect new frame %d:\n", frameInd);
+            frameInd ++;
+            for (int i = 0; i < 19; i ++) {
+                selectedFrameInds[i].push_back(curInds[i]);
+                printf("Stream %d select %dth frames, time shift%d\n", i, curInds[i],
+                    (int)(timeStamps[i][curInds[i]] - startTime) - curTime);
+                curInds[i]++;
+            }
+        }
+        else {
+
+            for (int i = 0; i < 19; i ++) {
+                uint64_t ts = timeStamps[i][curInds[i]] - startTime;
+                if (ts > curTime && timeStampDist(ts, curTime) > eps) {
+                    // do nothing
+                }
+                else {
+                    curInds[i]++;
+                }
+            }   
+        }
+
+        // check if last frame
+        for (int i = 0; i < 19; i ++) {
+            if (curInds[i] == timeStamps[i].size())
+                lastframe = true;
+        }
+        if (lastframe == true)
+            break;
+        
+        curTime += 100000 / 3;
+    }
     return 0;
 }
 
