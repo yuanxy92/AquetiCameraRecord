aaa0d4106ec327387dba63b437706b134734e396
yuanxy92 <yuanxy92@gmail.com>
2018-12-09 12:43:12 +0800

    chech the valid video length, which is not the same, we need to select frames again

--- FindSyncFrames.cpp
+++ FindSyncFrames.cpp
@@ -34,6 +34,10 @@ int readTimeStamps(char* dir, std::vector<std::vector<uint64_t>>& timeStamps) {
     return 0;
 }
 
+inline int timeStampDist(uint64_t t1, uint64_t t2) {
+    return t1 > t2 ? t1 - t2 : t2 - t1;
+}
+
 int extractSyncFrames(std::vector<std::vector<uint64_t>> timeStamps, 
     std::vector<std::vector<int>>& selectedFrameInds) {
     // go through all the first frame to find the one having largest time stamp
@@ -44,6 +48,31 @@ int extractSyncFrames(std::vector<std::vector<uint64_t>> timeStamps,
         }
     }
     printf("Timestamp of the first frame is %llu !\n", largestTimeStamp);
+    // find inds for each h264 stream
+    selectedFrameInds.resize(19);
+    uint64_t eps = 33000 / 2; 
+    for (int i = 0; i < 19; i ++) {
+        for (int ind = 0; ind < timeStamps[i].size(); ind ++) {
+            uint64_t dist = timeStampDist(largestTimeStamp, timeStamps[i][ind]);
+            if (dist < eps) {
+                if (ind < timeStamps[i].size() - 1) {
+                    uint64_t dist2 = timeStampDist(largestTimeStamp, timeStamps[i][ind + 1]);
+                    if (dist < dist2)
+                        selectedFrameInds[i].push_back(ind);
+                    else selectedFrameInds[i].push_back(ind + 1);
+                }
+                else selectedFrameInds[i].push_back(ind);
+                break;
+            }
+        }
+    }
+    // output information
+    for (int i = 0; i < 19; i ++) {   
+        printf("Stream %d has %lu frames, starts from index %d, has valid frames %d\n", i, timeStamps[i].size(),
+            selectedFrameInds[i][0], timeStamps[i].size() - selectedFrameInds[i][0]);
+    }
+
+    // start to find 
     return 0;
 }
 
